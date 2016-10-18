b7cb22a26db40e703aad461842ffc177f8c0ec26
zsw <291873236@qq.com>
2016-10-18 15:15:21 +0800

    test

---
+++ src/main/java/zsw/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+	
+	public static void main(){
+		
+		int  t = 0;
+		
+		int  y = 4;
+		
+		Test test = new Test ();
+		int j = test.add(t, y);
+		System.out.println(j);
+		
+	}
+	public int add(int x, int t) {
+		y = x + y;
+		return y;
+	}
+	
+	
+}
